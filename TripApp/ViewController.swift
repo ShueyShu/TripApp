4e689cf26ca55ef5743d2b6a966dd01b8b1f38a3
ShueyShu <artisticshuey@gmail.com>
2018-12-25 19:45:12 -0500

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by Shubhank Sahay Personal on 12/25/18.
+//  Copyright © 2018 Shubhank Sahay Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
